d57c18c576706c59a58f5c0289e4b8b7a378e696
Mqo <78250380+Mqondisi-Mavuso@users.noreply.github.com>
2022-08-22 23:45:21 +0800

    It's not finished yet

---
+++ Google_kickstart/Round E/mentor.py
@@ -0,0 +1,34 @@
+
+test_cases = int(input())
+case = 0
+sorted_list = []
+final_list = []
+
+while test_cases:
+    num_students = int(input())
+    case += 1
+    students = list(map(int, input().split()))              # read and save to a list
+
+    for s in students:
+        sorted_list.append(s)
+
+    sorted_list.sort(reverse=True)
+
+    for i in range(num_students - 1):
+        for j in range(num_students - 1):
+            if students[i] <= (sorted_list[j])/2:
+                final_list.append(sorted_list[j])
+
+            if j < num_students - 1:
+                if students[i] == sorted_list[j] and students[i] == sorted_list[j+1]:
+                    final_list.append(sorted_list[j])
+
+            
+            else:
+                continue
+
+    for final in final_list:
+        print(final)
+
+    # print(f"Case #{case}: {suc_matrix}")
+    test_cases -= 1
